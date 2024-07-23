62bdafb9bd185bcfcdd589ebc262b6269ad37e4a
Bharath Kannan <bharathkannan96@hotmail.com>
2024-07-23 19:17:53 +0100

    UX_Portfolio

--- js/index.js
+++ js/index.js
@@ -20,6 +20,7 @@ console.log("Your index.js file is loaded correctly!");
     scrollUpBtn.addEventListener("click", function () {
       document.body.scrollTop = 0; // For Safari
       document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+      console.log("You clicked on go to top!"); 
     });
 
     // Accordion 
